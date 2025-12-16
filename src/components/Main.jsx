127bdc4efd4a488497f11e0348ac6eacbaf99b40
aldodenegri <aldodenegri00@gmail.com>
2025-12-16 17:49:29 +0100

    parte 2 completa

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,17 +3,32 @@ import merchandise from '../assets/buy-comics-merchandise.png'
 import shop from '../assets/buy-comics-shop-locator.png'
 import visa from '../assets/buy-dc-power-visa.svg'
 import subscription from '../assets/buy-comics-subscriptions.png'
+import comics from '../components/comics.js'
 import './Main.css'
 function Main() {
     return (
-        <div id="main_div" class="main">
-            <ul>
-                <li><img src={digital_comics}></img><a href="#">DIGITAL COMICS</a></li>
-                <li><img src={merchandise}></img><a href="#">DC MERCHANDISE</a></li>
-                <li><img src={subscription}></img><a href="#">SUBSCRIPTION</a></li>
-                <li><img src={shop}></img><a href="#">COMIC SHOP LOCATOR</a></li>
-                <li><img src={visa}></img><a href="#">DC POWER VISA</a></li>
-            </ul>
+        <div>
+            <div id="main_div_products">
+
+                <div class="comics-grid">
+                    {comics.map((comic) => (
+                        <div id={comic.id} class="comic-card">
+                            <img src={comic.thumb} alt={comic.title} class="comic-thumb" />
+                            <h3>{comic.series}</h3>
+                        </div>
+                    ))}
+                </div>
+
+            </div>
+            <div id="main_div" class="main">
+                <ul>
+                    <li><img src={digital_comics}></img><a href="#">DIGITAL COMICS</a></li>
+                    <li><img src={merchandise}></img><a href="#">DC MERCHANDISE</a></li>
+                    <li><img src={subscription}></img><a href="#">SUBSCRIPTION</a></li>
+                    <li><img src={shop}></img><a href="#">COMIC SHOP LOCATOR</a></li>
+                    <li><img src={visa}></img><a href="#">DC POWER VISA</a></li>
+                </ul>
+            </div>
         </div>
     );
 };
